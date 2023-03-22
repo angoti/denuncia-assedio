393a133e603be2d3d6d47371014c3255b17a2b1c
Breno Eduardo <64919093+BrenoEduardo@users.noreply.github.com>
2023-03-22 17:54:51 -0300

    Delete AwesomeProject directory

--- AwesomeProject/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@react-native-community', 'plugin:react/jsx-runtime'],
-  rules: {
-    'react-native/no-inline-styles': 0,
-    'prettier/prettier': ['error', { 'no-inline-styles': false }],
-    "requireConfigFile": false
-  },
-};
