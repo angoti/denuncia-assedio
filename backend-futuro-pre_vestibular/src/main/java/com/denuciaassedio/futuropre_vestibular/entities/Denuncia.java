8cb96329d1b6c0d3f15d9acf625736124c9e7337
Edson Angoti Junior <angoti@gmail.com>
2023-04-17 20:15:36 +0000

    correções de biblioteca: use javax.persistence

--- backend-futuro-pre_vestibular/src/main/java/com/denuciaassedio/futuropre_vestibular/entities/Denuncia.java
+++ backend-futuro-pre_vestibular/src/main/java/com/denuciaassedio/futuropre_vestibular/entities/Denuncia.java
@@ -2,18 +2,19 @@ package com.denuciaassedio.futuropre_vestibular.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-
 import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "tb_denuncia")
